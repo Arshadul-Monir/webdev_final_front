e38e218d43f833c27cb5feecb5f0a84bad4d392a
Jason Jordan HOME <jasonajordan0@gmail.com>
2024-07-25 10:37:27 -0400

    Added react-rounter-dom with an example. We can now use branches

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+// Pages
+import ErrorPage from './pages/error-page.jsx';
+import Bananas from './pages/bananas.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    ErrorPage: <ErrorPage />
+  },
+  {
+    path: "/banana",
+    element: <Bananas />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>,
 )
