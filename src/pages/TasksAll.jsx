4631e9f44d85a57b04c439d874e9f7b8f9f40958
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2024-08-01 16:10:50 -0400

    made the task list into a table

--- src/pages/TasksAll.jsx
+++ src/pages/TasksAll.jsx
@@ -1,5 +1,17 @@
 export default function TaskPage()
 {
+    
+
+    return(
+        <div>
+            <p> niuts</p>
+            <TaskList></TaskList>
+        </div>
+    );
+}
+
+function TaskList()
+{
     const dummyTasks = [
         {id: 88, priority_lvl: 2, description : "ballz", complete: false, owner : "joe mama"},
         {id: 1, priority_lvl: 5, description : "invade poland", complete: true, owner : "ludwig van beethoven"},
@@ -7,14 +19,31 @@ export default function TaskPage()
         {id: 404, priority_lvl: 1000, description : "defend gotham", complete: false, owner : "the dark knight"}
     ];
 
-const listedTasks = dummyTasks.map((task) => (
-        <p>{task.id}, {task.priority_lvl}, {task.description}, {task.complete? "Complete" : "Incomeplete"}, owner</p>
+    const listedTasks = dummyTasks.map((task) => (
+        <tr>
+            <th>{task.id}</th>
+            <th>{task.priority_lvl}</th>
+            <th>{task.description}</th>
+            <th>{task.complete? "Complete" : "Incomeplete"}</th>
+            <th>{task.owner}</th>
+        </tr>
     ));
 
     return(
-        <div>
-            <p> niuts</p>
-            {listedTasks}
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Priority Level</th>
+                    <th>Description</th>
+                    <th>Completion Status</th>
+                    <th>Owner</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {listedTasks}
+            </tbody>
+        </table>
     );
 }
